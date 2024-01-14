92664dc6f733cf9888112dd8a4e77438b6a38eb8
surelymz <114205829+surelymz@users.noreply.github.com>
2024-01-14 10:06:05 -0500

    runningCode

--- DonationScreen.tsx
+++ DonationScreen.tsx
@@ -172,145 +172,145 @@ export default DonationScreen;
 
 
 
-// get App ID
-const getAppID = {
-  method: 'GET',
-  url: 'https://api.circle.com/v1/w3s/config/entity',
-  headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
-};
+// // get App ID
+// const getAppID = {
+//   method: 'GET',
+//   url: 'https://api.circle.com/v1/w3s/config/entity',
+//   headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
+// };
 
-axios
-  .request(getAppID)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-
-
-// create User
-
-const createUser = {
-  method: 'POST',
-  url: 'https://api.circle.com/v1/w3s/users',
-  headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'},
-  data: {userId: '<USER_ID>'}
-};
-
-axios
-  .request(createUser)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(getAppID)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 
 
+// // create User
 
-//acquire Token
-const acquireToken = {
-  method: 'POST',
-  url: 'https://api.circle.com/v1/w3s/users/token',
-  headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'},
-  data: {userId: '<USER_ID>'}
-};
+// const createUser = {
+//   method: 'POST',
+//   url: 'https://api.circle.com/v1/w3s/users',
+//   headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'},
+//   data: {userId: '<USER_ID>'}
+// };
 
-axios
-  .request(acquireToken)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-  //initialize user
-const initializeUser = {
-  method: 'POST',
-  url: 'https://api.circle.com/v1/w3s/user/initialize',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415',
-    'X-User-Token': '<USER_TOKEN>'
-  },
-  data: {idempotencyKey: '<IDEMPOTENCY_KEY>', blockchains: '[<BLOCKCHAIN>]'}
-};
-
-axios
-  .request(initializeUser)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(createUser)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 
 
-//checkWalletStatus
-const checkWalletStatus = {
-  method: 'GET',
-  url: 'https://api.circle.com/v1/w3s/wallets/__USER-ID__',
-  headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
-};
 
-axios
-  .request(checkWalletStatus)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// //acquire Token
+// const acquireToken = {
+//   method: 'POST',
+//   url: 'https://api.circle.com/v1/w3s/users/token',
+//   headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'},
+//   data: {userId: '<USER_ID>'}
+// };
+
+// axios
+//   .request(acquireToken)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+//   //initialize user
+// const initializeUser = {
+//   method: 'POST',
+//   url: 'https://api.circle.com/v1/w3s/user/initialize',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415',
+//     'X-User-Token': '<USER_TOKEN>'
+//   },
+//   data: {idempotencyKey: '<IDEMPOTENCY_KEY>', blockchains: '[<BLOCKCHAIN>]'}
+// };
+
+// axios
+//   .request(initializeUser)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 
-//fetchWalletIDandBalance
-const fetchWalletIDandBalance = {
-  method: 'GET',
-  url: 'https://api.circle.com/v1/w3s/wallets/__WALLET_ID__/balances',
-  headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
-};
 
-axios
-  .request(fetchWalletIDandBalance)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// //checkWalletStatus
+// const checkWalletStatus = {
+//   method: 'GET',
+//   url: 'https://api.circle.com/v1/w3s/wallets/__USER-ID__',
+//   headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
+// };
 
-//initiateTransaction
+// axios
+//   .request(checkWalletStatus)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
-const initiateTransaction = {
-  method: 'POST',
-  url: 'https://api.circle.com/v1/w3s/user/transactions/transfer',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415',
-    'X-User-Token': '<USER_SESSION_TOKEN>'
-  },
-  data: {
-    idempotencyKey: '<UNIQUE_UUID>',
-    userId: '<ID_OF_THE_USER_YOU_WANT_TO_USE>',
-    destinationAddress: '<DESTINATION_WALLET_ADDRESS>',
-    refId: '<ANY_REFERENCE>',
-    amounts: ['<AMOUNT_1>'],
-    feeLevel: 'HIGH',
-    tokenId: '<ID_OF_THE_TOKEN_YOU_WANT_TO_TRANSFER>',
-    walletId: '<ID_OF_PREVIOUSLY_GENERATED_WALLET>'
-  }
-};
 
-axios
-  .request(initiateTransaction)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// //fetchWalletIDandBalance
+// const fetchWalletIDandBalance = {
+//   method: 'GET',
+//   url: 'https://api.circle.com/v1/w3s/wallets/__WALLET_ID__/balances',
+//   headers: {'Content-Type': 'application/json', Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415'}
+// };
+
+// axios
+//   .request(fetchWalletIDandBalance)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+// //initiateTransaction
+
+// const initiateTransaction = {
+//   method: 'POST',
+//   url: 'https://api.circle.com/v1/w3s/user/transactions/transfer',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer TEST_API_KEY:6120ecd761ee5ef56d171dfecb1b6623:434725ee5de71344370e495ee372b415',
+//     'X-User-Token': '<USER_SESSION_TOKEN>'
+//   },
+//   data: {
+//     idempotencyKey: '<UNIQUE_UUID>',
+//     userId: '<ID_OF_THE_USER_YOU_WANT_TO_USE>',
+//     destinationAddress: '<DESTINATION_WALLET_ADDRESS>',
+//     refId: '<ANY_REFERENCE>',
+//     amounts: ['<AMOUNT_1>'],
+//     feeLevel: 'HIGH',
+//     tokenId: '<ID_OF_THE_TOKEN_YOU_WANT_TO_TRANSFER>',
+//     walletId: '<ID_OF_PREVIOUSLY_GENERATED_WALLET>'
+//   }
+// };
+
+// axios
+//   .request(initiateTransaction)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
